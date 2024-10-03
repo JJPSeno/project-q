4c42298a4144f650d95ca56158fce88ea7ea3acb
Jewel Josef P. Seno <seno.jeweljosef@gmail.com>
2024-10-03 21:00:49 +0800

    Updated generated obstacles; Added yellow modulation on floor

--- World/obstacles/obstacles.gd
+++ World/obstacles/obstacles.gd
@@ -2,7 +2,7 @@ extends StaticBody2D
 
 @export var MIN_POINTS := 3
 @export var MAX_POINTS := 6
-@export var DIMENSION_SIZE := 40.0
+@export var DIMENSION_SIZE := 200.0
 @onready var collision_polygon_2d: CollisionPolygon2D = $CollisionPolygon2D
 
 # Called when the node enters the scene tree for the first time.
@@ -14,9 +14,10 @@ func _ready() -> void:
 		var vector_point = Vector2(randf_range(-DIMENSION_SIZE,DIMENSION_SIZE),randf_range(-DIMENSION_SIZE,DIMENSION_SIZE))
 		vector_points_arr.append(vector_point)
 	vector_points_arr.sort_custom(sort_clockwise)
-	polygon_node.set_polygon(PackedVector2Array(vector_points_arr))
+	var convex = ConvexPolygonShape2D.new()
+	convex.set_point_cloud(PackedVector2Array(vector_points_arr))
+	polygon_node.polygon = convex.points
 	add_child(polygon_node)
-	print_debug(polygon_node.polygon)
 	collision_polygon_2d.polygon = polygon_node.polygon
 
 
