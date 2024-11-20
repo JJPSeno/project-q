c545cd3bb70029d6e0f243019a7ef5143c301873
Jewel Josef P. Seno <seno.jeweljosef@gmail.com>
2024-11-21 07:24:15 +0800

    Update spike; add controls ui

--- Player/spike/spike_projectile.gd
+++ Player/spike/spike_projectile.gd
@@ -3,11 +3,16 @@ extends Area2D
 const SPIKE_LINE_TEXTURE := preload("res://Assets/Player/spike/spike_link_sm.png")
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @export var player : CharacterBody2D
+@export var spike_speed := 200.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	EventBus.spike_hit.connect(_on_spike_hit)
 	EventBus.spike_traversed.connect(_on_spike_traversed)
+
+
+func _physics_process(delta: float) -> void:
+	position += transform.x * spike_speed * delta
 
 
 func _on_spike_hit() -> void:
